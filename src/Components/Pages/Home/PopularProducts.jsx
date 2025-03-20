df2da0f94409b984789a15c0f7bd55f4ab97945b
Hesam Mousavi <hesammousavi@Mac.lan>
2025-03-20 12:50:02 +0000

    added

--- src/Components/Pages/Home/PopularProducts.jsx
+++ src/Components/Pages/Home/PopularProducts.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./PopularProducts.css";
 import pic1 from "../../../Images/new/pic10.jpg";
-import pic2 from "../../../Images/new/pic2.jpg";
-import pic3 from "../../../Images/new/pic3.jpg";
-import pic4 from "../../../Images/new/pic12.jpg";
-import pic5 from "../../../Images/new/pic5.jpg";
-import pic6 from "../../../Images/new/pic11.jpg";
+import pic2 from "../../../Images/new/pic16.jpg";
+import pic8 from "../../../Images/new/pic8.jpg";
+import pic4 from "../../../Images/new/pic2.jpg";
+import pic5 from "../../../Images/new/pic11.jpg";
+import pic9 from "../../../Images/new/pic9.jpg";
 
 const products = [
-  { name: "Balloons", price: "£30", image: pic1 },
-  { name: "Birthday set up", price: "£30", image: pic2 },
-  { name: "Birthday set up", price: "£30", image: pic3 },
-  { name: "Proposal", price: "£30", image: pic4 },
-  { name: "Birthday set up", price: "£30", image: pic5 },
-  { name: "Birthday set up", price: "£30", image: pic6 },
+  { name: "Bloom and Bubbles", price: "£30", image: pic1 },
+  { name: "Luxury Natural Bouquet", price: "£30", image: pic2 },
+  { name: "Spring Bouquet", price: "£30", image: pic8 },
+  { name: "Pastel Hatbox", price: "£30", image: pic4 },
+  { name: "Personalised Balloons", price: "£30", image: pic5 },
+  { name: "Rose in Balloon", price: "£30", image: pic9 },
 ];
 
 const PopularProducts = () => {
